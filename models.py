55340972bd91eaad9dee0921ad898e959ac7a061
4723221 <4723221i@hgu.jp>
2026-01-26 13:28:19 +0900

    Create models.py

---
+++ models.py
@@ -0,0 +1,7 @@
+from tortoise.models import Model
+from tortoise import fields
+# TODOアイテムモデルの定義（Tortoise ORM）
+class TodoItem(Model):
+id = fields.IntField(pk=True)
+title = fields.CharField(max_length=1024)
+description = fields.CharField(max_length=1024,
